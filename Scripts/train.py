c3df859cc7b9b23d0e18fac281b68b21c9a37808
Tegisty Hailay <tigisthay13@gmail.com>
2022-09-13 20:38:26 +0300

    prompt engineering

---
+++ Scripts/train.py
@@ -0,0 +1,9 @@
+with open('results.txt', 'w') as result:
+      result.write('Large Language Model\n')
+      result.write('\n')     
+      result.write('GPT-2\n')
+      result.write('Best Score: 59.3%\n')
+      result.write('\n')  
+      result.write('GPT-3\n')
+      result.write('Best Score: 24.72%\n')
+      result.write('\n')  
